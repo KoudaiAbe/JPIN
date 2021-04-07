dea20d60656d15c89d01de79de76c273e6a2d7f9
koudai <idaukobear@gmail.com>
2021-04-07 23:11:34 +0900

    前置後置の動き

--- src/sample.java
+++ src/sample.java
@@ -1,6 +1,23 @@
 
 public class sample {
 	public static void main(String[] args){
-		System.out.println("Hello JPIN2");
+//		System.out.println("Hello JPIN2");
+//		int num;
+//		num = 10;
+//		System.out.println(num);
+		
+		int a = 10;
+		int b = a++ + ++a;
+
+		//a++
+		//int a = 11;
+		//int b = 10 + ++a;
+		
+		//++a
+		//int a = 11;
+		//int b = 10 + 12;
+		
+		System.out.println(b);//22
+		
 	}
 }
