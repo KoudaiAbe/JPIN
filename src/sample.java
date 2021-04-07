56d2607080474734e0ddb4f615f94d143e7f62c9
koudai <idaukobear@gmail.com>
2021-04-07 23:23:45 +0900

    型違いの計算について

--- src/sample.java
+++ src/sample.java
@@ -17,7 +17,13 @@ public class sample {
 		//int a = 11;
 		//int b = 10 + 12;
 		
-		System.out.println(b);//22
+//		System.out.println(b);//22
+	
+		String str1 = "Hi";
+		String str2 = str1 + 100 + 100;
+		String str3 = 100 + 100 + str1;
 		
+		System.out.println(str2);//Hi100100
+		System.out.println(str3);//200Hi
 	}
 }
