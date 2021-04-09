258ea96cc80562246f258bc8ea4d2d5c421be515
koudai <idaukobear@gmail.com>
2021-04-09 22:50:34 +0900

    JavaSilver2章問題のコード動作確認

---
+++ src/JavaSilver.java
@@ -0,0 +1,19 @@
+
+public class JavaSilver {
+	public static void main(String[] args) {
+//		String str = "aaaa";
+//		System.out.println(str.replace("aa", "b"));//bb
+		
+//		int val = 7;
+//		boolean flg = true;
+//		if(flg == true) {
+//			do {
+//				System.out.println(val);
+//			}
+//			while(val > 10);
+//		}
+//		
+		String str = "abcde";
+		System.out.println(str.charAt(str.length()));
+	}
+}
