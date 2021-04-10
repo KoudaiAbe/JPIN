b5291148dd69ad37e326c192a885e62509e5fdaf
koudai <idaukobear@gmail.com>
2021-04-10 23:12:55 +0900

    前置後置の計算確認とboolean型の比較

--- src/JavaSilver.java
+++ src/JavaSilver.java
@@ -13,7 +13,17 @@ public class JavaSilver {
 //			while(val > 10);
 //		}
 //		
-		String str = "abcde";
-		System.out.println(str.charAt(str.length()));
+//		String str = "abcde";
+//		System.out.println(str.charAt(str.length()));
+		
+//		int a = 10;
+//		int b = a++ + a + a-- - a-- + ++a;
+//		System.out.println(b);//32
+		
+
+//		boolean a = true;
+//		boolean b = true;
+//		System.out.println(a<=b);//boolean型で大なり小なりは使えない
+//		System.out.println(a < b);
 	}
 }
