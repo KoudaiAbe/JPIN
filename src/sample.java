5ef1d47079ade9234e8082c81f5a3b060a88a935
koudai <idaukobear@gmail.com>
2021-04-14 21:53:21 +0900

    拡張for文と配列

--- src/sample.java
+++ src/sample.java
@@ -14,19 +14,43 @@ public class sample {
 //			System.out.println("hello");
 //		}while(i < 5);
 		
+//		
+//		int i = 0;
+//		
+//		while(true) {
+//			i++;
+//			if (i == 3) {
+//				continue;
+//			}
+//			System.out.println(i);
+//	
+//			if(5 < i) {
+//				break;
+//			}
+//		}		
 		
-		int i = 0;
 		
-		while(true) {
-			i++;
-			if (i == 3) {
-				continue;
-			}
-			System.out.println(i);
-	
-			if(5 < i) {
-				break;
-			}
-		}		
+//		int[] array = new int[3];
+//		array[0] = 10;
+//		array[1] = 3;
+//		array[2] = 5;
+		
+		int[] array = {10,20,30,40,-50,8,10,11};
+		
+//		for(int i = 0;i < array.length;i++) {
+//			System.out.println(array[i]);
+//		}
+		
+//		for(int num : array) {
+//			System.out.println(num);
+//			
+//		}
+		
+//		while(array.length < 10) {
+//			System.out.println(array);
+//			break;
+//		}
+		
+		
 	}
 }
