04da8c5907bc68abde846b56b0d517a0ffc16228
koudai <idaukobear@gmail.com>
2021-04-14 20:53:21 +0900

    4/14　2重for

--- src/sample.java
+++ src/sample.java
@@ -1,22 +1,17 @@
 
 public class sample {
 	public static void main(String[] args){
-		
-		int i = 3;
-		
-		switch(i) {
-			case 1:System.out.println("one");
-					break;
+				
+		for(int i = 0;i < 3;i++) {
+			for(int j = 0;j < 3;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 			
-			case 2:System.out.println("two");
-					break;
-			
-			case 3:System.out.println("three");
-					break;
-			
-			default:System.out.println("other");
-					break;
+//			***
+//			***
+//			***
+
 		}
-		
 	}
 }
