2d40957916b2e5679f13e63814152db0f4f22e9b
koudai <idaukobear@gmail.com>
2021-04-14 23:33:28 +0900

    メソッドのスコープ

--- src/sample.java
+++ src/sample.java
@@ -1,56 +1,71 @@
 
 public class sample {
-	public static void main(String[] args){
-		
-//		int i = 0;
-//		
-////		条件分岐に入る前に判定する
-////		while(i < 5) {
-////			System.out.println("hello");
-////			i++;
-////		}
-////		最低一回実行する、条件の判定が実行後に判定される		
-//		do {
-//			System.out.println("hello");
-//		}while(i < 5);
-		
-//		
-//		int i = 0;
-//		
-//		while(true) {
-//			i++;
-//			if (i == 3) {
-//				continue;
-//			}
-//			System.out.println(i);
-//	
-//			if(5 < i) {
-//				break;
-//			}
-//		}		
-		
-		
-//		int[] array = new int[3];
-//		array[0] = 10;
-//		array[1] = 3;
-//		array[2] = 5;
-		
-		int[] array = {10,20,30,40,-50,8,10,11};
-		
-//		for(int i = 0;i < array.length;i++) {
-//			System.out.println(array[i]);
-//		}
-		
-//		for(int num : array) {
-//			System.out.println(num);
-//			
-//		}
-		
-//		while(array.length < 10) {
-//			System.out.println(array);
-//			break;
-//		}
-		
-		
+	
+	public static void test(int num) {
+		num = num * 2;
+		System.out.println(num);//200
 	}
+	
+	
+	public static void main(String[] args) {
+		int num = 100;
+		test(num);
+		System.out.println(num);//100
+	}
+	
+	//	public static void main(String[] args){
+//		
+////		int i = 0;
+////		
+//////		条件分岐に入る前に判定する
+//////		while(i < 5) {
+//////			System.out.println("hello");
+//////			i++;
+//////		}
+//////		最低一回実行する、条件の判定が実行後に判定される		
+////		do {
+////			System.out.println("hello");
+////		}while(i < 5);
+//		
+////		
+////		int i = 0;
+////		
+////		while(true) {
+////			i++;
+////			if (i == 3) {
+////				continue;
+////			}
+////			System.out.println(i);
+////	
+////			if(5 < i) {
+////				break;
+////			}
+////		}		
+//		
+//		
+////		int[] array = new int[3];
+////		array[0] = 10;
+////		array[1] = 3;
+////		array[2] = 5;
+//		
+////		int[] array = {10,20,30,40,-50,8,10,11};
+//		
+////		for(int i = 0;i < array.length;i++) {
+////			System.out.println(array[i]);
+////		}
+//		
+////		for(int num : array) {
+////			System.out.println(num);
+////			
+////		}
+//		
+////		while(array.length < 10) {
+////			System.out.println(array);
+////			break;
+////		}
+//		
+//		
+//		
+//		
+//	}
 }
